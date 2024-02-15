a3fde3725d46c303a22e6e2ed95004358837c7f4
Anastasia <nastushadem@mail.ru>
2024-02-15 16:47:08 +0500

    void CalculateArray

--- Program.cs
+++ Program.cs
@@ -8,3 +8,15 @@ void PrintArray(string[] arr)
     string str = "[" + String.Join(", ", arr) + "]";
     Console.WriteLine(str);
 }
+string[] CalculateArray(str[] arr)
+{
+    string[] resultArray = new string[0];
+    for (int i = 0; i < arr.Length;i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            Array.Resize(ref resultArray, resultArray.Length +1);
+            resultArray [resultArray.Length - 1] = arr[i];
+        }
+    }
+}
