dd8fd753106806d4d7a769fe3ced7c175acc09d0
Anastasia <nastushadem@mail.ru>
2024-02-15 16:39:24 +0500

    void PrinArray

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] startArray = { "Hello", "my", "teacher", "!", ":-)" };
+PrintArray(startArray);
+string[] resultArray = CalculateArray(startArray);
+PrintArray(resultArray);
+
+void PrintArray(string[] arr)
+{
+    string str = "[" + String.Join(", ", arr) + "]";
+    Console.WriteLine(str);
+}
